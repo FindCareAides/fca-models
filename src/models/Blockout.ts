d685e4ca235387c5fe62378cfc7da362a203b927
fob413 <oluwafunso.oluyole.balogun@gmail.com>
2025-11-09 10:46:32 -0500

    chore: adds organizationId to blockouts

--- src/models/Blockout.ts
+++ src/models/Blockout.ts
@@ -3,6 +3,7 @@ import type { BaseDocument } from "../types/common";
 
 export interface IBlockOut extends BaseDocument, Document {
   caregiverId: mongoose.Types.ObjectId;
+  organizationId: mongoose.Types.ObjectId;
   start: Date;
   end: Date;
   note?: string;
@@ -11,6 +12,7 @@ export interface IBlockOut extends BaseDocument, Document {
 const BlockOutSchema = new Schema<IBlockOut>(
   {
     caregiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+    organizationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Organization', required: true, index: true }
     start: { type: Date, required: true, index: true },
     end: { type: Date, required: true },
     note: String,
